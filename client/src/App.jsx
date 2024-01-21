6790e41d594fc66d76c585a8b603a6615f95c0e0
Taha-Tanveer <tahat1700@gmail.com>
2024-01-21 16:53:07 +0530

    header with routes

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Header from './components/Header'
+const App = () => {
+  
+  return (
+    <Router>
+<Header />
+<Routes>
+  <Route path='/' element={<Home />} />
+  <Route path='/sign-in' element={<SignIn />} />
+  <Route path='/sign-out' element={<SignOut />} />
+  <Route path='/about' element={<About />} />
+  <Route path='/profile' element={<Profile />} />
+</Routes>
+    </Router>
+  )
+}
+
+export default App
