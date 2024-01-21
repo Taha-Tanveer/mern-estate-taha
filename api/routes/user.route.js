3dc77b991fb9fc5dd9ad77258052bf23d236125e
Taha-Tanveer <tahat1700@gmail.com>
2024-01-21 22:40:05 +0530

    create a test api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { test } from "../controllers/user.controller.js";
+
+
+const userrouter = express.Router();
+userrouter.get("/test", test)
+
+
+export default userrouter;
